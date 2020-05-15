3bd8159b447c1a42a84897f39af136feb02f00b1
Riya Goyal <you@example.com>
2020-05-16 00:08:40 +0530

    add files

---
+++ todos.js
@@ -0,0 +1,28 @@
+$("ul").on("click" ,"li" ,function()
+{
+	$(this).toggleClass("comp");}
+
+);
+$("ul").on("click" ,"span" ,function(event)
+{
+	$(this).parent().fadeOut(500,function()
+		{
+			$(this).remove();
+		}
+		);
+	event.stopPropogation();
+
+});
+$("input").keypress(function(event)
+{
+	if(event.which===13)
+		{var todo=$(this).val();
+			$(this).val("");
+			$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>"+todo+"</li>")
+		}
+
+});
+$(".fa-plus").click(function()
+{
+	$("input[type='text']").fadeToggle()
+});
